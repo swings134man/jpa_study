049e7e7a685f8f435f200f4d0b41533c6f87ba54
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2022-09-22 17:25:46 +0900

    jpa-order java file add

--- jpa-order/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa-order/src/main/java/jpabook/jpashop/domain/Order.java
@@ -14,9 +14,41 @@ public class Order {
     @Column(name = "MEMBER_ID")
     private Long memberId; // 누가 주문했는지 확인해야하기 떄문.
 
-    private LocalDateTime orderDate; // 주문시각
+    private LocalDateTime orderDate; // 주문시각 -> DB상에선 ORDER_DATE, order_date(DB에 기본 생성형식) 형식임.
 
     @Enumerated(EnumType.STRING) // String 필수조건
     private OrderStatus status; // 주문 상태
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
--- jpa-order/src/main/java/jpabook/jpashop/jpaMain.java
+++ jpa-order/src/main/java/jpabook/jpashop/jpaMain.java
@@ -1,9 +1,35 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class jpaMain {
     public static void main(String[] args) {
 
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("unit_h2"); // manager factory
 
+        EntityManager em = emf.createEntityManager(); // manager
 
-    }
+        EntityTransaction tx = em.getTransaction(); //transaction
+        tx.begin();
+
+        // Transaction strart
+        try {
+
+            Order order = em.find(Order.class, 1L);
+            Long memberId = order.getMemberId();
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+
+    }//main
 }
