7cc44268eff7ce582d758ccc931ed11dadb45989
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-02-26 02:51:38 +0900

    JPQL 서브쿼리

--- jpa-basic/src/main/java/sixth_Query/jpql/Jpql_main_2.java
+++ jpa-basic/src/main/java/sixth_Query/jpql/Jpql_main_2.java
@@ -12,7 +12,7 @@ import java.util.List;
  * @date : 2023/02/23 1:03 AM
  * @author : SeokJun Kang(swings134@gmail.com)
  * @version : 1.0.0
- * @Description :
+ * @Description : JOIN , SubQuery
  ************/
 public class Jpql_main_2 {
     public static void main(String[] args) {
@@ -26,12 +26,12 @@ public class Jpql_main_2 {
             // Jp_member
             // 생성
 //                Jp_team team = new Jp_team();
-//                team.setName("A");
+//                team.setName("B");
 //                em.persist(team);
 //
 //                Jp_member member = new Jp_member();
-//                member.setUsername("A");
-//                member.setAge(10);
+//                member.setUsername("member3");
+//                member.setAge(20);
 //                member.changedTeam(team);
 //                em.persist(member);
 //
@@ -57,17 +57,35 @@ public class Jpql_main_2 {
              */
 
             // - 조인대상 필터링 : 회원과 팀을 조인하면서 팀이름이 A인 팀만 조인
-            String ql = "select m from Jp_member m left join m.team t on t.name = 'A'";
+//            String ql = "select m from Jp_member m left join m.team t on t.name = 'A'";
 //            List<Jp_member> resultList = em.createQuery(ql, Jp_member.class).getResultList();
 
 
             // 연관관계가 없는 엔티티 외부 조인
-            String ql2 = "select m from Jp_member m left join Jp_team t on m.username = t.name";
+//            String ql2 = "select m from Jp_member m left join Jp_team t on m.username = t.name";
 //            List<Jp_member> resultList = em.createQuery(ql2, Jp_member.class).getResultList();
 //            System.out.println("size: " + resultList.get(2).getUsername());
 //            for(Jp_member member : resultList){
 //                System.out.println(member);
 //            }
+
+            // ---------------------------------------- 서브쿼리 예제 ----------------------------------------
+//            Jp_member member = new Jp_member();
+
+            // 예제 1번 - 팀 'A'에 속해 있는 회원
+//            String q1 = "select m from Jp_member m where exists(select t from Jp_team t where t.name = 'A')";
+//            List<Jp_member> resultList1 = em.createQuery(q1, Jp_member.class).getResultList();
+//            System.out.println("res : "+ resultList1);
+
+            // 예제 2번 - 어떤 팀이든 팀에 소속된 회원
+//            String q2 = "select m from Jp_member m where m.team = ANY(select t from Jp_team t)";
+//            List<Jp_member> resultList2 = em.createQuery(q2, Jp_member.class).getResultList();
+//            System.out.println("res : " + resultList2);
+
+            // 예제 3번 - 나이가 평균보다 많은 회원
+//            String q3 = "select m from Jp_member m where m.age > (select avg(m2.age) from Jp_member m2)";
+//            List<Jp_member> resultList3 = em.createQuery(q3, Jp_member.class).getResultList();
+//            System.out.println("res : " + resultList3);
 
             tx.commit();
         }catch (Exception e) {
